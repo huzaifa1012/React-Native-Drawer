27da15e17b7db3569c790f0dbae559e11eb6fc2c
huzaifahux <www.pakteach@gmail.com>
2023-03-29 01:01:41 +0500

    drawer added

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
+import 'react-native-gesture-handler';
 
-export default function App() {
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function B() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>B</Text>
+  )
+}
+function MyDrawer({ navigation }) {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Screen2" component={Screen2} />
+      <Drawer.Screen name="B" component={B} />
+    </Drawer.Navigator>
+  );
+}
+
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button 
+        title='Screen Mov'
+        onPress={() => { navigation.openDrawer(); }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+function Screen2() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen 2</Text>
+    </View>
+  );
+}
+
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Screen2" component={Screen2} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="MyDrawer">
+        <Drawer.Screen name="MyDrawer" component={MyDrawer} />
+        {/* <Stack.Screen name="Screen2" component={Screen2} /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default App;
